9a0d19c60bb17ba06e080ce3b1ef88ba3ce224d6
EuanHoll <ehollidge@hotmail.com>
2023-12-01 16:13:30 +0100

    Day 1

---
+++ adventofcode/2023/day_1.py
@@ -0,0 +1,40 @@
+import json
+import os
+import re
+
+resources_path = os.path.join(os.path.dirname(__file__), "resources")
+
+
+def open_input_file(path: str) -> list:
+    with open(path, "r") as f:
+        lines = f.readlines()
+    return lines
+
+
+def get_first_and_last_number_in_str(string: str) -> int:
+    # Find all numbers in string as a list of singke digit stringd
+    numbers = re.findall(r"\d", string)
+
+    return numbers[0] + numbers[-1]
+
+
+def get_first_and_last_from_list(list_of_strings: list) -> list:
+    return [get_first_and_last_number_in_str(string) for string in list_of_strings]
+
+
+def cli():
+    lines = open_input_file(os.path.join(resources_path, "day_1_input.txt"))
+    numbers = get_first_and_last_from_list(lines)
+
+    with open(os.path.join(resources_path, "day_1_output.txt"), "w") as f:
+        for number in numbers:
+            f.write(f"{number}\n")
+
+
+if __name__ == "__main__":
+    import time
+
+    start = time.time()
+    cli()
+    end = time.time()
+    print(f"Runtime: {end - start}")
